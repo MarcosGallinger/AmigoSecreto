6da9f66cdd7c948dde5a901357f69db28215837f
Marcos Gallinger <marcosdavidgallinger@gmail.com>
2025-02-21 11:23:53 -0300

    crear funciones

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listaAmigos = [];
+let Nombre = document.getElementById('listaAmigos');
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento)
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function agregarAmigo() {
+    let amigoUsuario = document.getElementById("amigo").value;
+    document.querySelector('#amigo').value = '';
+    listaAmigos.push(amigoUsuario);
+    let amigosCadena = "";
+    for (let i = 0; i < listaAmigos.length; i++) {
+        amigosCadena += listaAmigos[i] + "\n";
+     }
+     asignarTextoElemento('ul', amigosCadena);
+    }
+
+function sortearAmigo(){
+    let sorteoAmigoSecreto = Math.floor(Math.random() * listaAmigos.length);
+    asignarTextoElemento('ul', listaAmigos[sorteoAmigoSecreto]);
+    limpiarCaja;
+    return;    
+}
+
+
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+    let ul = document.querySelector('ul');
+    ul.innerHTML = '';
+}
+  
